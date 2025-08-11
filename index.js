d77ec6810f9013a0fed099f819fcd98d5db7db2a
chamollamohit <mohitchamolla@gmail.com>
2025-08-11 11:39:47 +0530

    fixed bug

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { connectDB } from "./src/db/db.js";
 import path  from "path";
 import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
-
+import cors from "cors";
 dotenv.config("./env")
 const PORT = process.env.PORT
 const app = express()
@@ -18,6 +18,9 @@ app.use(cookieParser())
 app.set("view engine", "ejs")
 app.set('views', path.join(__dirName,"views"))
 app.use(express.static(path.join(__dirName,"public")))
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}))
 
 // Impprorting Routes
 import staticRoute from "./src/routes/static.route.js";
